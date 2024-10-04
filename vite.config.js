5584ca428829999c19ac9ce8115e2954cd669cd9
Mohammed Rafeeq <tarahadvertising@gmail.com>
2024-10-04 21:27:52 +0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/project/",
   build: {
     rollupOptions: {
       input: {
         main: "./index.html",
+        branding: "./service_branding.html",
+        sign: "./service_signage.html",
+        graphics: "./service_graphics.html",
+        bags: "./service_bags.html",
+        flags: "./service_flags.html",
+        gifts: "./service_gifts.html",
+        contact: "./contact.html",
       },
     },
   },
